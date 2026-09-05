c7fd9e384666e68adee6ee2440a9f35efe389150
Ashwin M S <msashwin825@gmail.com>
2026-09-05 11:46:43 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,32 @@
+
+
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        HashMap<Character, Integer> map1 = new HashMap<>();
+
+        for (char c : s1.toCharArray()) {
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+
+            HashMap<Character, Integer> map2 = new HashMap<>();
+
+            for (int j = i; j < i + s1.length(); j++) {
+
+                char c = s2.charAt(j);
+
+                if (map1.containsKey(c)) {
+                    map2.put(c, map2.getOrDefault(c, 0) + 1);
+                }
+            }
+
+            if (map1.equals(map2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
